40d4ccdd85f11b034054f9106c64d43cfd99c469
Rancemxn <209220634+Rancemxn@users.noreply.github.com>
2026-02-09 00:49:39 +0800

    Update webcv.py

--- src/webcv.py
+++ src/webcv.py
@@ -235,7 +235,7 @@ prefs:
         self.session = GeckoSession()
         self.session.open(self.runtime)
         self.webview.setSession(self.session)
-        self.session.loadUri(os.path.abspath('web_canvas.html'))
+        self.session.loadUri('about:support')
         
         activity.setContentView(self.webview)
         
