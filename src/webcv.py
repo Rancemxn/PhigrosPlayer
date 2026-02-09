479e15485d64f301530d334acc85123257b68d3f
Rancemxn <209220634+Rancemxn@users.noreply.github.com>
2026-02-09 12:33:26 +0800

    Update webcv.py

--- src/webcv.py
+++ src/webcv.py
@@ -50,6 +50,7 @@ if checksys == 'Android':
     GeckoRuntime = autoclass('org.mozilla.geckoview.GeckoRuntime')
     GeckoSession = autoclass('org.mozilla.geckoview.GeckoSession')
     activity = autoclass('org.kivy.android.PythonActivity').mActivity
+    LayoutParams = autoclass('android.view.ViewGroup$LayoutParams')
 
 class JsApi:
     def __init__(self) -> None:
@@ -213,14 +214,7 @@ prefs:
   gfx.work-around-driver-bugs: false
   gfx.canvas.accelerated: true
   gfx.canvas.accelerated.force-enabled: true
-  
-  layers.gpu-process.force-enabled: true
-  security.sandbox.gpu.level: 0
-
-
   gl.ignore-warnings: true
-  webgl.force-enabled: true
-  layers.acceleration.force-enabled: true
 """)
         logging.info('Initializing Geckoview')
         builder = Builder()
