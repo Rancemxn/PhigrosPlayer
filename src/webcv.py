eb753c7f06c12a37e352ac4447d5251bad1ba890
Rancemxn <209220634+Rancemxn@users.noreply.github.com>
2026-02-09 12:38:54 +0800

    Update webcv.py

--- src/webcv.py
+++ src/webcv.py
@@ -235,6 +235,9 @@ prefs:
         self.webview.setSession(self.session)
         self.session.loadUri('about:support')
         
+        match_parent = LayoutParams.MATCH_PARENT
+        params = LayoutParams(match_parent, match_parent)
+        activity.setContentView(self.webview, params)
         activity.setContentView(self.webview)
         
     
