973d0fb56d43b333d502dc6505c055c9a24cf49b
Rancemxn <209220634+Rancemxn@users.noreply.github.com>
2026-02-11 14:11:34 +0800

    Update webcv.py

--- src/webcv.py
+++ src/webcv.py
@@ -205,14 +205,12 @@ class WebCanvas:
         with open('org.qaqfei.phigrosplayer.phigrosplayer-geckoview-config.yaml', 'w', encoding='utf-8') as f:
             f.write("""
 env:
-  MOZ_LOG: "Canvas:5,GfxAdapter:5,Acceleration:5,GLContext:5,Compositor:5,SharedSurface:5,WebRender:5,GPU:5"
+  MOZ_LOG: "Acceleration:5,GLContext:5,Compositor:5,WebRender:5,GPU:5"
 
 prefs:
 
   gfx.canvas.accelerated: true
   gfx.canvas.accelerated.force-enabled: true
-  gfx.canvas.accelerated.debug: true
-  gfx.canvas.accelerated.allow-in-parent: true
                     
 
   gfx.webrender.debug.profiler: true
