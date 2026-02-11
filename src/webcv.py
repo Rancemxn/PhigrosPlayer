04dd5d9204e649208dc34c47ae92d30fb5979315
Rancemxn <209220634+Rancemxn@users.noreply.github.com>
2026-02-11 21:08:08 +0800

    Update webcv.py

--- src/webcv.py
+++ src/webcv.py
@@ -218,6 +218,8 @@ prefs:
   gfx.canvas.accelerated.debug: true
   gfx.canvas.accelerated.allow-in-parent: true
   gfx.webrender.debug.profiler: true
+  gfx.webrender.fallback.software: false
+  gfx.webrender.software: false
 
   gfx.canvas.accelerated.max-surface-size: 999999
   gfx.canvas.accelerated.max-size: 999999
