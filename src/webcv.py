3914556ea97f2721c3880f52afd3ffab494d788b
Rancemxn <209220634+Rancemxn@users.noreply.github.com>
2026-02-11 16:11:13 +0800

    Update webcv.py

--- src/webcv.py
+++ src/webcv.py
@@ -212,8 +212,6 @@ prefs:
   gfx.canvas.accelerated: true
   gfx.canvas.accelerated.force-enabled: true
   gfx.logging.level: 5
-  gfx.canvas.max-size: 32767
-  gfx.canvas.accelerated.max-size: 32767
   gfx.webrender.debug.profiler: true
 """)
         logging.info('Initializing Geckoview')
